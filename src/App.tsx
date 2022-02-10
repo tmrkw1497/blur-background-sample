d595a8148f7f3899ec9648ceda0012f7b3202b85
b1018043 <grey.fox.0417@gmail.com>
2022-02-11 03:50:10 +0900

    feat:モデル読み込みの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
+import React,{useEffect,useRef, useState} from 'react';
+import '@tensorflow/tfjs-backend-webgl';
+import * as bodyPix from '@tensorflow-models/body-pix';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [loaded,setLoaded] = useState<boolean>(false);
+  useEffect(()=>{
+    const constraints: MediaStreamConstraints = {
+      audio: false,
+      video:{
+        width: 600,
+        height: 400
+      }
+    };
+    navigator.mediaDevices.getUserMedia(constraints).then((stream)=>{
+      videoRef.current!.srcObject = stream;
+    })
+    setLoaded(true);
+  },[]);
+  useEffect(()=>{
+    videoRef.current!.onloadeddata = (ev=>{
+      const loadAndPredict = async()=>{
+        const net = await bodyPix.load({
+          architecture: 'MobileNetV1',
+          outputStride: 16,
+          multiplier: 0.75,
+          quantBytes: 2
+        });
+        const segmentation = await net.segmentPerson(videoRef.current!,{
+          flipHorizontal: false,
+          internalResolution: "medium",
+          segmentationThreshold: 0.7
+        });
+        console.log(segmentation);
+      }
+      loadAndPredict();
+    })
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video
+        ref={videoRef}
+        autoPlay
+        playsInline
+        muted
+        width="600px"
+        height="400px"
+      />
     </div>
   );
 }
